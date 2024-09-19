c2cae5773db4d600b4cbfb5e64055fdd42f7afc6
Aditi <aditin@andrew.cmu.edu>
2024-09-19 16:56:27 -0400

    first commit! working milestone 1

---
+++ LFD.py
@@ -0,0 +1,93 @@
+# LFD (Local Fault Detector) Pseudocode
+import socket
+import select
+from socket import timeout
+from time import localtime, strftime, time, sleep
+import sys
+import threading
+
+last_heartbeat_acked = time()
+
+def send_heartbeat(heartbeat_freq, lfd_socket):
+    global last_sent_time, heartbeat_count
+    if time() - last_sent_time >= heartbeat_freq: 
+        heartbeat = f"<LFD1, S1, {heartbeat_count}, heartbeat>"
+        print(f"[{strftime("%Y-%m-%d %H:%M:%S", localtime())}] [{heartbeat_count}] LFD1 sending heartbeat to S1")
+        try:
+            last_sent_time = time()
+            lfd_socket.sendall(heartbeat.encode())
+
+            # increment heartbeat number
+            heartbeat_count += 1
+        except Exception as e:
+            pass
+
+def receive_heartbeat(lfd_socket, heartbeat_freq):
+    global last_received_time
+    to_read_buffer, _, _ = select.select([lfd_socket], [],[], heartbeat_freq/3) # TODO: change timeout later, for non blocking operation
+    if to_read_buffer:
+        try:
+            heartbeat_ack = lfd_socket.recv(1024).decode("utf-8")
+            # if it's a heartbeat message, update the last heartbeat received time
+            if "heartbeat" in heartbeat_ack:
+                heartbeat_count_str = heartbeat_ack.strip('<').split(',')[2].strip()
+                print(f"[{strftime("%Y-%m-%d %H:%M:%S", localtime())}] [{heartbeat_count_str}] LFD1 receives heartbeat ACK from S1")
+                last_received_time = time()
+        except Exception as e: # Not sure if I should be exception handling
+            pass
+
+def send_receive_check_heartbeat(lfd_socket, heartbeat_freq, heartbeat_timeout):
+    global last_sent_time, heartbeat_count, last_received_time
+    heartbeat_count = 1
+
+    last_received_time = time()
+    last_sent_time = time()
+
+
+    while True:
+        send_heartbeat(heartbeat_freq, lfd_socket)
+        receive_heartbeat(lfd_socket, heartbeat_freq)
+                    
+            
+        # CHECK TIMEOUT
+        if time() - last_received_time > heartbeat_timeout:
+            print("elapsed time from last received msg: ", time() - last_received_time)
+
+            print(f"[{strftime("%Y-%m-%d %H:%M:%S", localtime())}] S1 has died.")
+            heartbeat_count = 1
+            break
+            
+            
+
+
+def run_LFD(heartbeat_freq):
+    # initialize heartbeat info
+    heartbeat_timeout = 2*heartbeat_freq
+
+    while True:
+        try:
+            # init lfd socket and connect
+            lfd_socket = socket.socket()
+            lfd_socket.settimeout(heartbeat_timeout)
+            s1_port = 6000
+            s1_ip = '127.0.0.1'
+            lfd_socket.connect((s1_ip, s1_port))
+
+            send_receive_check_heartbeat(lfd_socket, heartbeat_freq, heartbeat_timeout)
+
+            # shouldn't try to wait for the "s1 has died" to join because I haven't set a break
+        except Exception as e:  
+            print(f"Error while trying to reconnect to server: {e}")
+            sleep(heartbeat_timeout)    # idk if this is the correct behavior
+    
+
+
+if __name__ == "__main__":
+    run_LFD(int(sys.argv[1]))
+        
+
+       
+
+   
+    
+
---
+++ client.py
@@ -0,0 +1,49 @@
+# CLIENT pseudocode
+
+import socket, select
+from time import localtime, strftime, time, sleep
+import sys
+
+def run_client(client_id, client_freq):
+    # initialize client
+    c = socket.socket()
+    # init server id
+    server_id = 1
+    # connect client with server
+    s1_port = 6000
+    s1_ip = '127.0.0.1'
+    c.connect((s1_ip, s1_port))
+    # initialize request number
+    c_request_num = 0
+
+    # while True
+    while True:
+        # keep track of request num
+        c_request_num += 1
+
+        # send requests to server
+        # request = <C1, S1, request_num, request> 
+        request_str = f"<C{client_id}, S{server_id}, {c_request_num}, Hello Server!>"
+        # send request
+        c.send(request_str.encode())
+
+        # get timestamp
+        sent_timestamp_str = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        # print [timestamp] Sent 'request'
+        print(f"[{sent_timestamp_str}] Sent {request_str}")
+
+            
+        # receive reply from server:
+        # reply = whatever server sends
+
+        # todo: do i need nonblocking here??????
+        reply = c.recv(1024).decode()   # try catch
+        # print [timestamp] Received 'reply'
+        recv_timestamp_str = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        print(f"[{recv_timestamp_str}] Received {reply}")
+
+        # wait 2 seconds before sending another message
+        sleep(client_freq)
+
+if __name__ == "__main__":
+    run_client(int(sys.argv[1]), int(sys.argv[2]))    # give the client id as a commandline parameter AND the frequency with which client should send messages
