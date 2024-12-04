7c973632887ec6a017d54f1a5b820563edd0f8aa
yaoxx3 <yaox3@andrew.cmu.edu>
2024-12-04 17:43:12 -0500

    ip config for bash scripts and ssh keys

--- S3.sh
+++ S3.sh
@@ -4,4 +4,4 @@
 #python3 server.py <server_index (1,2,3)> <checkpoint_freq> <server_ip1> <server_ip2> <server_ip3> 
 
 echo "running server 3"
-python3 server.py 3 2 18.236.81.208 35.89.143.40 172.31.37.26
+python3 server.py 3 2 3.80.211.150 44.201.110.34 172.31.13.20
