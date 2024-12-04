5b3a5e970c81536d66ec7da2f8262749e784f505
yaoxx3 <yaox3@andrew.cmu.edu>
2024-12-04 17:40:22 -0500

    wrong branch

--- S2.sh
+++ S2.sh
@@ -4,4 +4,4 @@
 #python3 server.py <server_index (1,2,3)> <checkpoint_freq> <server_ip1> <server_ip2> <server_ip3> 
 
 echo "running server 2"
-python3 server.py 2 5 172.26.80.245 172.26.80.245 172.26.80.245
+python3 server.py 2 5 3.80.211.150 172.31.88.181 18.227.134.0
--- S3.sh
+++ S3.sh
@@ -4,4 +4,4 @@
 #python3 server.py <server_index (1,2,3)> <checkpoint_freq> <server_ip1> <server_ip2> <server_ip3> 
 
 echo "running server 3"
-python3 server.py 3 5 172.26.80.245 172.26.80.245 172.26.80.245
+python3 server.py 3 5 3.80.211.150 44.201.110.34 172.31.13.20
