cc11e2d0d2a0d348204fa1a2cb9792daa09872e7
jd362 <dong77@wisc.edu>
2024-12-03 23:11:59 -0500

    Reduce checkpoint frequency

--- S1.sh
+++ S1.sh
@@ -4,4 +4,4 @@
 #python3 server.py <server_index (1,2,3)> <checkpoint_freq> <server_ip1> <server_ip2> <server_ip3> 
 
 echo "running server 1" 
-python3 server.py 1 5 172.31.35.155 35.89.143.40 34.217.83.10
+python3 server.py 1 2 172.31.35.155 35.89.143.40 34.217.83.10
--- S2.sh
+++ S2.sh
@@ -4,4 +4,4 @@
 #python3 server.py <server_index (1,2,3)> <checkpoint_freq> <server_ip1> <server_ip2> <server_ip3> 
 
 echo "running server 2"
-python3 server.py 2 5 18.236.81.208 172.31.46.129 34.217.83.10
+python3 server.py 2 2 18.236.81.208 172.31.46.129 34.217.83.10
